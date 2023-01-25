83c52ae311a986a4c6461f0571fd93f1a99c5039
Andrea Barberio <insomniac@slackware.it>
2023-01-25 13:24:14 +0100

    Initial commit
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

---
+++ cmd/neato/login.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	flagLoginCode        string
+	flagLoginInteractive bool
+)
+
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "Log in to the Neato cloud",
+	Run: func(cmd *cobra.Command, args []string) {
+		isInteractive := flagLoginInteractive
+		email := viper.GetString("email")
+		code := flagLoginCode
+		var err error
+		if email == "" {
+			if isInteractive {
+				email, err = prompt("Type your Neato account email")
+				if err != nil {
+					log.Fatalf("Failed to read email from stdin")
+				}
+				email = strings.TrimSuffix(email, "\n")
+			} else {
+				log.Fatalf("No e-mail provided in non-interactive mode")
+			}
+		}
+		if code == "" {
+			log.Printf("No code provided, sending a login request")
+			if err := loginRequestCode(email); err != nil {
+				log.Fatalf("Login request failed: %v", err)
+			}
+			if !isInteractive {
+				log.Printf("Please check your e-mail for the login code, and re-run this command with the --code flag")
+				os.Exit(0)
+			} else {
+				code, err = prompt("Type the login code you have received via e-mail")
+				if err != nil {
+					log.Fatalf("Failed to read code from stdin")
+				}
+				code = strings.TrimSuffix(code, "\n")
+			}
+		}
+		// get the token. At this point we have e-mail and login code
+		token, err := loginRequestToken(email, code)
+		if err != nil {
+			log.Fatalf("Failed to get login token: %v", err)
+		}
+		fmt.Println(token)
+	},
+}
+
+func initLoginCmd() {
+	var (
+		flagLoginEmail string
+	)
+
+	loginCmd.Flags().StringVarP(&flagLoginEmail, "email", "e", "", "Email address of the Neato account")
+	loginCmd.Flags().StringVarP(&flagLoginCode, "code", "C", "", "Verification code that is sent to your e-mail")
+	loginCmd.Flags().BoolVarP(&flagLoginInteractive, "interactive", "i", false, "Interactive login")
+
+	flagMapping := map[string]string{
+		"email": "email",
+	}
+	for flagName, configDirective := range flagMapping {
+		if err := viper.BindPFlag(configDirective, loginCmd.Flags().Lookup(flagName)); err != nil {
+			log.Fatalf("Failed to bind flag --%s to config directive %s: %v", flagName, configDirective, err)
+		}
+	}
+}
+
+func loginRequestCode(email string) error {
+	uri := "https://mykobold.eu.auth0.com/passwordless/start"
+	body := map[string]string{
+		"send":       "code",
+		"email":      email,
+		"client_id":  "KY4YbVAvtgB7lp8vIbWQ7zLk3hssZlhR",
+		"connection": "email",
+	}
+	// ignore response, we assume that an HTTP 200 is good enough
+	// TODO print response if in debug mode
+	_, err := loginRequest(uri, body)
+	return err
+}
+
+func loginRequestToken(email, code string) (string, error) {
+	uri := "https://mykobold.eu.auth0.com/oauth/token"
+	type tokenResponse struct {
+		AccessToken string `json:"access_token"`
+		IDToken     string `json:"id_token"`
+		Scope       string `json:"scope"`
+		ExpiresIn   int    `json:"expires_in"`
+		TokenType   string `json:"token_type"`
+	}
+	body := map[string]string{
+		"prompt":       "login",
+		"grant_type":   "http://auth0.com/oauth/grant-type/passwordless/otp",
+		"scope":        "openid email profile read:current_user",
+		"locale":       "en",
+		"otp":          code,
+		"source":       "vorwerk_auth0",
+		"platform":     "ios",
+		"audience":     "https://mykobold.eu.auth0.com/userinfo",
+		"username":     email,
+		"client_id":    "KY4YbVAvtgB7lp8vIbWQ7zLk3hssZlhR",
+		"realm":        "email",
+		"country_code": "DE",
+	}
+	resp, err := loginRequest(uri, body)
+	if err != nil {
+		return "", err
+	}
+	var tr tokenResponse
+	if err := json.Unmarshal(resp, &tr); err != nil {
+		return "", fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+	return tr.AccessToken, nil
+}
+
+func loginRequest(uri string, dataMap map[string]string) ([]byte, error) {
+	data, err := json.Marshal(dataMap)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal request data: %w", err)
+	}
+	req, err := http.NewRequest(http.MethodPost, uri, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("HTTP request failed: %w", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read HTTP body: %w", err)
+	}
+	// TODO print body in debug mode
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("expected HTTP 200 OK, got %s", resp.Status)
+	}
+	return body, nil
+}
+
+func prompt(text string) (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(text + " > ")
+	return reader.ReadString('\n')
+}
---
+++ cmd/neato/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path"
+
+	"github.com/kirsle/configdir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const progname = "neato"
+
+var (
+	defaultConfigFile = path.Join(configdir.LocalConfig(progname), "config.yml")
+
+	flagConfigFile string
+	flagToken      string
+)
+
+var rootCmd = &cobra.Command{
+	Use:              progname,
+	Short:            "neato is a CLI for BotVac's Neato cloud API",
+	Args:             cobra.MinimumNArgs(1),
+	TraverseChildren: true,
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVarP(&flagConfigFile, "config", "c", defaultConfigFile, "Authentication token")
+	rootCmd.PersistentFlags().StringVarP(&flagToken, "token", "t", "", "Authentication token")
+
+	// flag-name to config-directive mapping
+	flagMapping := map[string]string{
+		"token": "token",
+	}
+	for flagName, configDirective := range flagMapping {
+		if err := viper.BindPFlag(configDirective, rootCmd.PersistentFlags().Lookup(flagName)); err != nil {
+			log.Fatalf("Failed to bind flag --%s to config directive %s: %v", flagName, configDirective, err)
+		}
+	}
+
+	rootCmd.AddCommand(loginCmd)
+	initLoginCmd()
+}
+
+func initConfig() {
+	viper.SetConfigFile(flagConfigFile)
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Printf("Using config file '%s'\n", viper.ConfigFileUsed())
+	}
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
